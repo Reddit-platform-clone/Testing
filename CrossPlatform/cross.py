6cd464b46abe584143007b82ac9bc2cec6accf8a
Youssefemad88 <youssefemad881@hotmail.com>
2024-03-25 07:23:42 +0200

    wrong inputs

--- CrossPlatform/cross.py
+++ CrossPlatform/cross.py
@@ -10,6 +10,7 @@ driver.implicitly_wait(10)
 
 # Locators: 
 value_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
+pass_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
 username_Xpath_signup = "//android.widget.EditText"
 
 # VALID SIGN UP WITH EMAIL
@@ -44,4 +45,52 @@ def signup_email():
     driver.quit()
 
 
-#signup_email()  # if you want to test continue with email process uncomment this
+def signup_wrongemail():
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue with email").click()
+    sleep(3)
+
+    email_field = driver.find_element(by=AppiumBy.XPATH, value=value_Xpath_signup)
+    email_field.click()
+    sleep(1)
+    email_field.send_keys('youssefemadhotmail.com')
+    sleep(3)
+
+    pass_field = driver.find_element(by=AppiumBy.XPATH, value=pass_Xpath_signup)
+    pass_field.click()
+    pass_field.send_keys("eMAD1234")
+    sleep(3)
+    driver.implicitly_wait(10)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue").click()
+    sleep(3)
+
+    driver.quit()
+
+
+def signup_invalidpass():
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue with email").click()
+    sleep(3)
+
+    email_field = driver.find_element(by=AppiumBy.XPATH, value=value_Xpath_signup)
+    email_field.click()
+    sleep(1)
+    email_field.send_keys('youssefemad@hotmail.com')
+    sleep(3)
+
+    pass_field = driver.find_element(by=AppiumBy.XPATH, value=pass_Xpath_signup)
+    pass_field.click()
+    sleep(1)
+    pass_field.send_keys("eMAD12")
+    sleep(3)
+    driver.implicitly_wait(10)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue").click()
+    sleep(3)
+
+    driver.quit()
+
+
+# Uncomment the function you want
+#signup_email()
+#signup_wrongemail()
+#signup_invalidpass()
