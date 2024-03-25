37824539f17e507c8df45cd41e3328bcf4899a56
Youssefemad88 <youssefemad881@hotmail.com>
2024-03-25 08:14:05 +0200

    Login and Signup done

--- CrossPlatform/cross.py
+++ CrossPlatform/cross.py
@@ -8,10 +8,12 @@ from time import sleep
 driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(desired_caps))
 driver.implicitly_wait(10)
 
-# Locators: 
+# locators
 value_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
 pass_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
+pass_Xpath_login = "//android.widget.ScrollView/android.widget.EditText[2]"
 username_Xpath_signup = "//android.widget.EditText"
+
 
 # VALID SIGN UP WITH EMAIL
 def signup_email():
@@ -23,8 +25,7 @@ def signup_email():
     sleep(1)
     email_field.send_keys('youssefemad@hotmail.com')
     sleep(3)
-
-    pass_field = driver.find_element(by=AppiumBy.XPATH, value=value_Xpath_signup)
+    pass_field = driver.find_element(by=AppiumBy.XPATH, value=pass_Xpath_signup)
     pass_field.click()
     sleep(1)
     pass_field.send_keys("eMAD1234")
@@ -90,7 +91,53 @@ def signup_invalidpass():
     driver.quit()
 
 
+def login():
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Login").click()
+    sleep(3)
+
+    email_field = driver.find_element(by=AppiumBy.XPATH, value=value_Xpath_signup)
+    email_field.click()
+    sleep(1)
+    email_field.send_keys('youssefemad@hotmail.com')
+    sleep(3)
+    pass_field = driver.find_element(by=AppiumBy.XPATH, value=pass_Xpath_login)
+    pass_field.click()
+    sleep(1)
+    pass_field.send_keys("eMAD1234")
+    sleep(3)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue").click()
+    sleep(3)
+
+    driver.quit()
+
+
+def login_fp():
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Login").click()
+    sleep(3)
+
+    email_field = driver.find_element(by=AppiumBy.XPATH, value=value_Xpath_signup)
+    email_field.click()
+    sleep(1)
+    email_field.send_keys('youssefemad@hotmail.com')
+    sleep(3)
+
+    forgot_pass = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Forgot password?')
+    forgot_pass.click()
+    sleep(3)
+
+    email_or_user_field = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText')
+    email_or_user_field.click()
+    sleep(1)
+    email_or_user_field.send_keys('youssefemad@hotmail.com')
+    sleep(2)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Reset Password").click()
+    driver.quit()
+
 # Uncomment the function you want
-#signup_email()
-#signup_wrongemail()
-#signup_invalidpass()
+# signup_email()
+# signup_wrongemail()
+# signup_invalidpass()
+# login()
+# login_fp()
