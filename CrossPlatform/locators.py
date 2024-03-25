a0bbea30acc361e1db5c2c7ba67c15144b2b01e9
Youssefemad88 <youssefemad881@hotmail.com>
2024-03-25 05:30:27 +0200

    New locators

--- CrossPlatform/locators.py
+++ CrossPlatform/locators.py
@@ -2,10 +2,12 @@ from appium import webdriver
 from typing import Any, Dict
 
 desired_caps: Dict[str, Any] = {
-  'platformName': 'Android',
-  'deviceName': 'YourDeviceName',
-  'appPackage': 'com.reddit.reddit',
+    "platformName": "Android",
+    "appium:platformVersion": "13",
+    "appium:deviceName": "emulator-5554",
+    "appium:automationName": "UIAutomator2",
+    "appium:app": "C:\\Users\\Emad\\Desktop\\Cross-Platform-main\\sarakel\\build\\app\\outputs\\apk\\debug\\app-debug.apk"
 }
 
-appium_server_url = 'http://localhost:4723'
+url = 'http://localhost:4723'
   
