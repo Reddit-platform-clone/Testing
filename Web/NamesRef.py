939596cbea6e90c89d0ec9551cdc84bb6410ea0b
Seif Alsayed <144794216+SeifAlsayed02@users.noreply.github.com>
2024-03-26 23:38:48 +0200

    Reference file

---
+++ Web/NamesRef.py
@@ -0,0 +1,17 @@
+#Reference file for names and Pathes
+
+################login #################
+login_email = "TestingTeam@yopmail.com"
+login_password = "Testing@2025"
+login_username = "TestingTeam2025"
+loginButton="login-button"
+EmailSpace="email"
+PaswordSpace="password"
+loginConfirmButton="login-button"
+
+#################Sign up ##############
+signupButton="signup"
+ConfirmButtonXPATH="signup-button"
+sigupEmailSpace="register-email"
+signupPasswordSpace="register-password"
+sigupConfirmEmailSpace="confirm-email"
