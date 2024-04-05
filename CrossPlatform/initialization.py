a9ed6ae6f116358771cca88e05754a575ac6b479
Youssefemad88 <youssefemad881@hotmail.com>
2024-04-05 15:10:42 +0200

    Updated Locators

--- CrossPlatform/initialization.py
+++ CrossPlatform/initialization.py
@@ -1,4 +1,3 @@
-from appium import webdriver
 from typing import Any, Dict
 
 # Appium driver initialization 
@@ -12,3 +11,18 @@ desired_caps: Dict[str, Any] = {
 }
 
 url = 'http://localhost:4723'
+
+value_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]"
+pass_Xpath_signup = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]"
+createC_Xpath = "//android.widget.EditText"
+post_Xpath_title = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"
+post_Xpath_body = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"
+menu_Xpath_home = "//android.widget.FrameLayout[@resource-id='android:id/content']/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[1]"
+pass_Xpath_login = "//android.widget.ScrollView/android.widget.EditText[2]"
+username_Xpath_signup = "//android.widget.EditText"
+home_Xpath = "//android.widget.TextView[@content-desc='Home feed' and @resource-id='toolbar_feed_type_label']"
+pop_Xpath = "(//android.view.View[@resource-id='toolbar_feed_type_item'])[2]"
+watch_Xpath = "(//android.view.View[@resource-id='toolbar_feed_type_item'])[3]"
+latest_Xpath = "(//android.view.View[@resource-id='toolbar_feed_type_item'])[4]"
+join_Xpath = "//android.view.View[@resource-id='post_join_button']"
+upvote_Xpath = "(//android.view.View[@resource-id='post_footer'])[2]/android.view.View[1]"
