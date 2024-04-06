4a79be66a5f4304e1db496913e302834a5901127
Seif Alsayed <144794216+SeifAlsayed02@users.noreply.github.com>
2024-04-06 23:25:31 +0200

    Update NamesRef.py

--- Web/NamesRef.py
+++ Web/NamesRef.py
@@ -15,3 +15,14 @@ ConfirmButtonXPATH="signup-button"
 sigupEmailSpace="register-email"
 signupPasswordSpace="register-password"
 sigupConfirmEmailSpace="confirm-email"
+
+############Create Post#################
+Create_Button = "//*[@id='create-post']/span/span[2]"
+Post_Body_field = "//*[@id='post-composer_bodytext']/shreddit-composer/div/p"
+Title_field = "//*[@id='innerTextArea']"
+Post_Submit_Button = "//*[@id='submit-post-button']"
+
+########Navigation Bar##################
+Home_button = "//*[@id='reddit-logo']"
+OpenChat = "//*[@id='header-action-item-chat-button']"
+OpenInbox = "//*[@id='mini-inbox-tooltip']/span/faceplate-tracker/faceplate-tooltip/button"
