af775a0ae7e61e52cee66bfac4c7da28e934b3e9
SeifAlsayed02 <seifalsayed0202@gmail.com>
2024-04-16 00:50:08 +0200

    Updating names

--- Web/NamesRef.py
+++ Web/NamesRef.py
@@ -27,3 +27,24 @@ Home_button = "//*[@id='reddit-logo']"
 OpenChat = "//*[@id='header-action-item-chat-button']"
 OpenInbox = "//*[@id='mini-inbox-tooltip']/span/faceplate-tracker/faceplate-tooltip/button"
 
+########OAuth#################
+google_button = "//*[@id='container']/div/div[2]/span[1]"
+
+######CreateCommunity############
+nav_bar_button = "//*[@id='navbar-menu-button']"
+Create_Community_button = "//reddit-header-large/faceplate-dialog[@id='menu']/div[1]/div[1]/reddit-sidebar-nav[1]/nav[1]/faceplate-expandable-section-helper[1]/details[1]/faceplate-auto-height-animator[1]/div[1]/left-nav-create-community-button[1]"
+Community_name_field = "/html/body/shreddit-app/community-creation-modal//faceplate-modal/faceplate-form/faceplate-text-input//label/div/span/span[1]/input"
+Community_Submit = "/html/body/shreddit-app/community-creation-modal//faceplate-modal/faceplate-form/div[2]/button[2]"
+
+#######MyProfile###############
+Profile_Icon = "//*[@id='expand-user-drawer-button']/span/span/span/span/img"
+View_Profile = "//*[@id='user-drawer-content']/ul[1]/faceplate-tracker[1]/li/a/span[1]/span[2]/span[1]"
+Profile_Overview_button = "//*[@id='profile-feed-tabgroup']/a[1] "
+Profile_Posts_button = "//*[@id='profile-feed-tabgroup']/a[2] "
+Profile_Comments_button = "//*[@id='profile-feed-tabgroup']/a[3] "
+Profile_Saved_button = "//*[@id='profile-feed-tabgroup']/a[4] "
+Profile_Hidden_button = "//*[@id='profile-feed-tabgroup']/a[5] "
+Profile_Upvoted_button = "//*[@id='profile-feed-tabgroup']/a[6] "
+Profile_Downvoted_button = "//*[@id='profile-feed-tabgroup']/a[7] "
+
+
