887bfb9647a7b12cf01f5f77d12c58a34f1d58f4
Youssefemad88 <youssefemad881@hotmail.com>
2024-05-09 16:07:04 +0300

    Settings & Logout

--- CrossPlatform/sideBar.py
+++ CrossPlatform/sideBar.py
@@ -61,3 +61,76 @@ def social():
     driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Add").click()
     sleep(2)
     driver.quit()
+
+def settings():
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.sideBar_Xpath).click()
+    sleep(2)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Settings").click()
+    sleep(2)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Update email address").click()
+    sleep(1)
+    email = driver.find_element(by=AppiumBy.XPATH, value="//android.widget.EditText")
+    email.click()
+    email.send_keys("youssefemad@hotmail.com")
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue").click()
+    sleep(4)
+
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Change password").click()
+    sleep(1)
+
+    old = driver.find_element(by=AppiumBy.XPATH, value=initialization.oldpass_Xpath)
+    old.click()
+    sleep(1)
+    old.send_keys("1234567890")
+    sleep(1)
+
+    new = driver.find_element(by=AppiumBy.XPATH, value=initialization.newpass_Xpath)
+    new.click()
+    sleep(1)
+    new.send_keys("123456789")
+
+    conf = driver.find_element(by=AppiumBy.XPATH, value=initialization.confpass_Xpath)
+    conf.click()
+    sleep(1)
+    conf.send_keys("123456789")
+
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.changepass_Xpath).click()
+    sleep(4)
+
+    driver.find_element(by=AppiumBy.XPATH, value="//android.view.View[@content-desc='Gender']").click()
+    driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Male").click()
+    sleep(1)
+
+    chat = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="""Chat and messaging permisions""")
+    chat.click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.anyy_Xpath).click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.older_Xpath).click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.nobody_Xpath).click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.noomess_Xpath).click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.back_Xpath).click()
+    sleep(1)
+
+
+def logout():
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.profilee_Xpath).click()
+    sleep(1)
+    driver.find_element(by=AppiumBy.XPATH, value=initialization.logout_Xpath).click()
+    sleep(3)
+
+    driver.quit()
+
+# Uncomment the function you want:
+
+# saved()
+# history()
+# social()
+# settings()
+# logout()
