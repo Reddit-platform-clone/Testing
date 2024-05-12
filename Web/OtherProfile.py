676e24649a738ad3c553d5e1d901da39c3aa34a2
SeifAlsayed02 <seifalsayed0202@gmail.com>
2024-05-12 10:11:54 +0300

    OtherUser

--- Web/OtherProfile.py
+++ Web/OtherProfile.py
@@ -1,8 +1,8 @@
-from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 import NamesRef
 import NormalLogin
@@ -30,12 +30,27 @@ def OtherProfile():
     # Wait until the search bar appears and then search for the specified username
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, NamesRef.search_bar)))
     search_bar = driver.find_element(By.XPATH, NamesRef.search_bar)
-    search_bar.clear()
+    search_bar.click()
     sleep(5)
-    search_bar.send_keys("Mission_Anteater1023")
-    search_bar.send_keys(Keys.RETURN)
 
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, NamesRef.users_search_bar)))
+    user_search_bar = driver.find_element(By.XPATH, NamesRef.users_search_bar)
+
+    # Create an ActionChains object to perform mouse hover
+    actions = ActionChains(driver)
+
+    # Hover over the user avatar
+    actions.move_to_element(user_search_bar).perform()
+
+    user_search_bar.send_keys("hafez")
+    sleep(5)
+
+
+    first_result = driver.find_element(By.XPATH, NamesRef.first_user_profile)
+    first_result.click()
+    sleep(5)
     ########## Functionality under construction ###############
 
     # Close the browser window
     driver.close()
+OtherProfile()
